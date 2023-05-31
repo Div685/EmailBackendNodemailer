acfd78c0336223ddddebab3b7fbbd0c24b8cb541
Div685 <divyeshpatel685@gmail.com>
2023-05-31 13:29:36 +0300

    Add server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const emailRoutes = require("./routes/emailRoutes");
+
+const app = express();
+dotenv.config();
+
+const cors = require("cors");
+const corsOption = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+
+app.use(cors()); // Use this after the variable declration
+
+app.use(express.json()); // tell the server to accept the json data from frontend
+
+app.use("/email", emailRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
